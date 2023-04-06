6ed23661615da37e2ea45b30fc88f1caf14dc2ab
Valencia-08 <57954825+Valencia-08@users.noreply.github.com>
2023-04-06 12:15:57 +0100

    addded fade out cover animation to index page

--- js/script.js
+++ js/script.js
@@ -23,7 +23,14 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "grid";
   dots[slideIndex-1].className += " active";
 }
 
+// $(".slideshow-container").on("swiperight", function(){
+//   $(".prev").click();
+// })
+
+// $(".slideshow-container").on("swipeleft", function(){
+//   $(".next").click();
+// })
